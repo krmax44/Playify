19772c8332501fbc2201aef3144f5047f65b58fe
Max <hi@krmax44.de>
2017-06-27 20:25:52 +0200

    v0.2
    More information in the release notes

--- background.js
+++ background.js
@@ -1,28 +1,38 @@
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        if (details.url.startsWith("https://open.spotify.com/")) {
-            var parser = document.createElement("a");
-            parser.href = details.url;
+  function(details) {
+    if (details.url.startsWith("https://open.spotify.com/")) {
+      var parser = document.createElement("a");
+      parser.href = details.url;
 
-            var requestBase = 'https://krmax44.de/playify/?type=';
-            var requestId = parser.pathname.split("/")[2];
-            var requestType = '';
+      if (parser.hash == "#nogpm") {
+        return;
+      }
 
-            if (parser.pathname.startsWith("/album/")) {
-                requestType = "album";
-            } else if (parser.pathname.startsWith("/artist/")) {
-                requestType = "artist";
-            } else if (parser.pathname.startsWith("/track/")) {
-                requestType = "track";
-            }
+      var requestBase = "https://krmax44.de/playify/?type=";
+      var requestId = parser.pathname.split("/")[2];
+      var requestType = "";
+      var requestExtra = "";
+      var playlistRegex = /\/[a-zA-Z0-9.]+\/playlist\/[a-zA-Z0-9]+/ig;
 
-            if (!!requestType) {
-              return { redirectUrl: requestBase + requestType + "&q=" + requestId };
-            }
+      if (parser.pathname.startsWith("/album/")) {
+        requestType = "album";
+      } else if (parser.pathname.startsWith("/artist/")) {
+        requestType = "artist";
+      } else if (parser.pathname.startsWith("/track/")) {
+        requestType = "track";
+      } else if (playlistRegex.exec(parser.pathname)) {
+        requestType = "playlist";
+        requestExtra = parser.pathname.split("/")[4];
+      }
 
-        }
-    }, {
-        urls: ["*://*.spotify.com/*"],
-        types: ["main_frame"]
-    }, ["blocking"]
+      if (!!requestType) {
+        return { redirectUrl: requestBase + requestType + "&q=" + requestId + "&r=" + requestExtra };
+      }
+
+    }
+  }, {
+    urls: ["*://*.spotify.com/*"],
+    types: ["main_frame"]
+
+  }, ["blocking"]
 );
