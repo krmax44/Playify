adb03d1fd4feb96c13a85d6854cfc2d986c00086
Max <hi@krmax44.de>
2017-06-28 20:21:30 +0200

    Backend v0.3
    Backend server in PHP

---
+++ _backend/domains.php
@@ -0,0 +1,26 @@
+<?php
+function getLink ($search) {
+  $domains = array();
+  $domains["list"] = array("gpm","amazon", "youtube");
+  $domains["gpm"] = "https://play.google.com/music/listen#/sr/#s#";
+  $domains["amazon"] = "https://music.amazon#e#/search/#s#";
+  $domains["youtube"] = "https://youtube.com/results?search_query=#s#";
+  $domains["amazontlds"]= array(".com",".de",".co.uk",".at");
+  
+  if (!in_array($_GET["d"], $domains["list"])) {
+    $domain = "gpm";
+  }
+  else {
+    $domain = $_GET["d"];
+  }
+  
+  if (!in_array($_GET["e"], $domains["amazontlds"])) {
+    $tld = ".com";
+  }
+  else {
+    $tld = $_GET["e"];
+  }
+
+  return str_replace("#s#", urlencode($search), str_replace("#e#", $tld, $domains[$domain]));
+}
+?>
